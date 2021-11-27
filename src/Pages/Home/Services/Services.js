0263c0e9790c0d442244fd37a243f019aee242bb
Mahinur Rahman <78277455+mahi1212@users.noreply.github.com>
2021-11-27 20:31:10 +0600

    Updated link

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,7 @@ import Service from '../Service/Service';
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('https://fierce-hamlet-00780.herokuapp.com/services')
+        fetch('https://lit-brushlands-74039.herokuapp.com/services')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
@@ -14,7 +14,7 @@ const Services = () => {
             {/* <h2 className='text-center text-uppercase text-danger py-2'>Our Services</h2> */}
             <Row xs={1} md={3} className="gx-4 gy-5 pt-3 px-5">
                 {
-                    services.map(service => <Service service={service}></Service>)
+                    services.map(service => <Service service={service} id={service.id}></Service>)
                 }
             </Row>
         </div>
