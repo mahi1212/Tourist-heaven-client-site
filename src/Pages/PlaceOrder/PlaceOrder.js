4a6bebd54d1b510e740b8f5811b223a6abf86937
Mahinur Rahman <78277455+mahi1212@users.noreply.github.com>
2021-11-27 20:32:10 +0600

    Updated link

--- src/Pages/PlaceOrder/PlaceOrder.js
+++ src/Pages/PlaceOrder/PlaceOrder.js
@@ -2,11 +2,23 @@ import React from 'react';
 import axios from 'axios'
 import { useForm } from "react-hook-form";
 import './PlaceOrder.css'
+import { useParams } from 'react-router';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const PlaceOrder = () => {
+    const [service, setService] = useState([])
+    const { orderId } = useParams()
+    // Fetch service
+    useEffect(() => {
+        fetch(`https://lit-brushlands-74039.herokuapp.com/services/${orderId}`)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [orderId])
+
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
-        axios.post('https://fierce-hamlet-00780.herokuapp.com/myOrder', data)
+        axios.post('https://lit-brushlands-74039.herokuapp.com/myorder', data)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('Added successfully')
@@ -17,8 +29,9 @@ const PlaceOrder = () => {
     }
     return (
         <div className='full-form'>
-            <form className='form-design' style={{ backgroundColor: '#9c9c', padding:'1rem' }} onSubmit={handleSubmit(onSubmit)}>
+            <form className='form-design' style={{ backgroundColor: '#9c9c', padding: '1rem' }} onSubmit={handleSubmit(onSubmit)}>
                 <h2>Place your order</h2>
+                <input {...register("serviceName")} value={service.name} defaultValue={'Service id :'+ orderId }/>
                 <input {...register("name", { required: true, maxLength: 20 })} placeholder='Name' />
                 <input {...register("address")} placeholder="Address" />
                 <input {...register("phone")} placeholder="Phone Number" />
